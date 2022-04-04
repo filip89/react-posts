b9338b775b7c3d24f2020907d6708ed34f9a4ba0
Filip Mikić <filip187m@gmail.com>
2022-04-04 20:12:53 +0200

    implement new react root api

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { greetingPrefix } from './config';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
     <BrowserRouter>
         <App greetIngPrefix={greetingPrefix} />
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
 );
